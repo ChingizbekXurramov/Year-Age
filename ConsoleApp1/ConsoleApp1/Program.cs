4e1d653c851801f3cdc12e5f57e07777b3f3b855
Chingizbek <128830130+ChingizbekXurramov@users.noreply.github.com>
2023-06-05 00:36:02 +0300

    Age

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter your Birth (year/month/day): ");
+                DateTime birthDate = DateTime.Parse(Console.ReadLine());
+
+                DateTime currentDate = DateTime.Now;
+                int age = currentDate.Year - birthDate.Year;
+
+                if (birthDate > currentDate.AddYears(-age))
+                {
+                    age--;
+                }
+
+                Console.WriteLine($"You are {age} years old");
+
+                TimeSpan span = currentDate - birthDate;
+                Console.WriteLine($"Your date of birth: {birthDate.Day:D2}/{birthDate.Month:D2}/{birthDate.Year} and" +
+                    $" {span.TotalDays:N0} days have passed since that day");
+
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Please put dates between >> / <<");
+                Main(args);
+            }
+        }
+    }
+}
